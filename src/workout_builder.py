8112ff23c90d4f2fc792dd7cbb6e3df3366a63fc
Kelley Neubauer <kelleyneubauer@gmail.com>
2021-03-25 19:04:26 -0700

    cleaning code smells. improving class encapsulation

--- src/workout_builder.py
+++ src/workout_builder.py
@@ -90,6 +90,11 @@ class View:
             last_block.frm.grid_forget()
             last_block.frm.destroy()
 
+    def get_last_block(self):
+        if self.exercise_blocks:
+            return self.exercise_blocks[-1]
+        return None
+
 
 class ExerciseBlock:
     labels = [('classification', 22), ('exercise name', 37), ('sets', 7), ('reps', 7), ('intensity', 7), ('video link', 22)]
@@ -135,6 +140,11 @@ class ExerciseBlock:
             last_exercise.frm.grid_forget()
             last_exercise.frm.destroy()
 
+    def get_last_exercise(self):
+        if self.exercises:
+            return self.exercises[-1]
+        return None
+
 
 class Exercise:
     def __init__(self, master, exercise_num):
@@ -166,51 +176,116 @@ class Exercise:
         self.btn_clear = tk.Button(master=self.frm, text='clear')
         self.btn_clear.grid(row=0, column=7, sticky='w')
 
+    def get_classification(self):
+        return self.cmb_classification.get()
+
+    def get_name(self):
+        return self.cmb_exercise_name.get()
+
+    def get_sets(self):
+        return self.spn_sets.get()
+
+    def get_reps(self):
+        return self.spn_reps.get()
+
+    def get_intensity(self):
+        return self.spn_intensity.get()
+
+    def get_link(self):
+        return self.cmb_video_link.get()
+
+    def set_classification_text(self, value):
+        self.cmb_classification.set(value)
+
+    def set_classification_values(self, values):
+        self.cmb_classification['values'] = values
+
+    def set_name_text(self, value):
+        self.cmb_exercise_name.set(value)
+
+    def set_name_values(self, values):
+        self.cmb_exercise_name['values'] = values
+
+    def set_sets_text(self, value):
+        self.spn_sets.delete(0, 'end')
+        self.spn_sets.insert(0, value)
+
+    def set_reps_text(self, value):
+        self.spn_reps.delete(0, 'end')
+        self.spn_reps.insert(0, value)
+
+    def set_intensity_text(self, value):
+        self.spn_intensity.delete(0, 'end')
+        self.spn_intensity.insert(0, value)
+
+    def set_link_text(self, value):
+        self.cmb_video_link.set(value)
+
+    def set_link_values(self, values):
+        self.cmb_video_link['values'] = values
+        self._show_first_combo_value(self.cmb_video_link)
+
+    def _show_first_combo_value(self, combobox):
+        if combobox['values']:
+            combobox.current(0)
+
 
 class Controller:
     def __init__(self):
         self.root = tk.Tk()
         self.model = Model()
         self.view = View(self.root)
-        self._bind_save_and_load_buttons()
-        self._bind_add_and_remove_block_buttons()
+        self._bind_launch_screen_events()
 
-    def _bind_add_and_remove_block_buttons(self):
-        self._bind_add_block_button()
-        self._bind_remove_block_button()
+    def _bind_launch_screen_events(self):
+        self._bind_button(self.view.btn_save, self._save_workout)
+        self._bind_button(self.view.btn_load, self._load_workout)
+        self._bind_button(self.view.btn_add_block, self._add_block)
+        self._bind_button(self.view.btn_remove_block, self._remove_block)
 
-    def _bind_add_block_button(self):
-        self.view.btn_add_block.bind('<Button>', self._add_block)
+    def _bind_button(self, button, handler):
+        button.bind('<Button>', handler)
 
-    def _bind_remove_block_button(self):
-        self.view.btn_remove_block.bind('<Button>', self._remove_block)
+    def _bind_combobox(self, combobox, handler):
+        combobox.bind('<<ComboboxSelected>>', handler)
 
     def _add_block(self, event=None):
         self.view.add_block()
-        new_block = self.view.exercise_blocks[-1]
+        new_block = self.view.get_last_block()
         self._bind_block_events(new_block)
 
     def _bind_block_events(self, block):
-        block.btn_add_exercise.bind('<Button>', lambda event, identifier=block: self._add_exercise(event, identifier))
-        block.btn_remove_exercise.bind('<Button>', lambda event, identifier=block: self._remove_exercise(event, identifier))
+        self._bind_button(block.btn_add_exercise,
+                          lambda event, group=block:
+                          self._add_exercise(event, group))
+        self._bind_button(block.btn_remove_exercise,
+                          lambda event, group=block:
+                          self._remove_exercise(event, group))
 
     def _add_exercise(self, event, block):
         block.add_exercise()
-
-        new_exercise = block.exercises[-1]
+        new_exercise = block.get_last_exercise()
         self._bind_exercise_events(new_exercise)
-        self._populate_exercise_comboboxes(new_exercise)
+        self._initialize_exercise_comboboxes(new_exercise)
 
     def _bind_exercise_events(self, exercise):
-        self._bind_exercise_clear_button(exercise)
-        self._bind_exercise_classification_combo(exercise)
-        self._bind_exercise_name_combo(exercise)
-        self._bind_exercise_view_link_button(exercise)
+        self._bind_button(exercise.btn_clear,
+                          lambda event, group=exercise:
+                          self._clear_onclick(event, group))
+        self._bind_button(exercise.btn_view_link,
+                          lambda event, group=exercise:
+                          self._view_onclick(event, group))
+        self._bind_combobox(exercise.cmb_classification,
+                            lambda event, group=exercise:
+                            self._classification_onclick(event, group))
+        self._bind_combobox(exercise.cmb_exercise_name,
+                            lambda event, group=exercise:
+                            self._exercise_name_onclick(event, group))
 
-    def _populate_exercise_comboboxes(self, exercise):
-        self._populate_exercise_classification_combo(exercise, self.model.get_exercise_categories())
-        self._populate_exercise_name_combo(exercise, self.model.get_exercise_names())
-        self._populate_exercise_video_links_combo(exercise, [])      
+    def _initialize_exercise_comboboxes(self, exercise):
+        exercise.set_classification_values(self.model.get_exercise_categories())
+        exercise.set_name_values(self.model.get_exercise_names())
+        exercise.set_link_values([])
 
     def _remove_exercise(self, event, block):
         block.remove_last_exercise()
@@ -218,21 +293,42 @@ class Controller:
     def _remove_block(self, event):
         self.view.remove_last_block()
 
-    def _bind_save_and_load_buttons(self):
-        self.view.btn_save.bind('<Button>', self._save_onclick)
-        self.view.btn_load.bind('<Button>', self._load_onclick)
-
     def _remove_all_blocks(self):
+        for block in self.view.exercise_blocks:
+            self._remove_all_exercises_in_block(block)
         while self.view.exercise_blocks:
             self.view.remove_last_block()
-            # am I removing exercises recursive with the child call in remove_last_block function?
-            # or are they just evaporating into the ether?
 
     def _remove_all_exercises_in_block(self, block):
         while block.exercises:
             block.remove_last_exercise()
 
-    def _save_onclick(self, event):
+    def _classification_onclick(self, event, exercise):
+        exercise.set_name_text('')
+        exercise.set_link_text('')
+        exercise.set_name_values(self.model.get_exercises_of_category(exercise.cmb_classification.get()))
+
+    def _exercise_name_onclick(self, event, exercise):
+        exercise.set_link_text('')
+        exercise.set_link_values(self.model.get_exercise_video_links(exercise.cmb_exercise_name.get()))
+
+    def _view_onclick(self, event, exercise):
+        webbrowser.open_new(exercise.get_link())
+
+    def _clear_onclick(self, event, exercise):
+        exercise.set_classification_text('')
+        exercise.set_name_text('')
+        exercise.set_sets_text(0)
+        exercise.set_reps_text(0)
+        exercise.set_intensity_text(0)
+        exercise.set_link_text('')
+        self._initialize_exercise_comboboxes(exercise)
+
+
+
+
+
+    def _save_workout(self, event):
         filename = self.view.ent_header_name.get()
         with open(filename + '.csv', mode='w', newline='') as output:
             output_writer = csv.writer(output, delimiter=',')
@@ -252,7 +348,7 @@ class Controller:
                     link = exercise.cmb_video_link.get()
                     output_writer.writerow([block_number, exercise_number, block_name, classification, exercise_name, sets, reps, intensity, link])
 
-    def _load_onclick(self, event):
+    def _load_workout(self, event):
         self._remove_all_blocks()
 
         filename = self.view.ent_header_name.get()
@@ -288,58 +384,9 @@ class Controller:
                 new_exercise.spn_intensity.insert(0, item[7])
                 new_exercise.cmb_video_link.set(item[8])
 
-    def _populate_exercise_classification_combo(self, exercise, values):
-        exercise.cmb_classification['values'] = values
-
-    def _populate_exercise_name_combo(self, exercise, values):
-        exercise.cmb_exercise_name['values'] = values
-
-    def _populate_exercise_video_links_combo(self, exercise, values):
-        exercise.cmb_video_link['values'] = values
-
-    def _bind_exercise_classification_combo(self, exercise):
-        exercise.cmb_classification.bind('<<ComboboxSelected>>', lambda event, row=exercise: self._classification_onclick(event, row))
-
-    def _bind_exercise_name_combo(self, exercise):
-        exercise.cmb_exercise_name.bind('<<ComboboxSelected>>', lambda event, row=exercise: self._exercise_name_onclick(event, row))
-
-    def _classification_onclick(self, event, exercise):
-        exercise.cmb_exercise_name.set('')
-        exercise.cmb_exercise_name['values'] = self.model.get_exercises_of_category(exercise.cmb_classification.get())
-        exercise.cmb_video_link.set('')
-
-    def _exercise_name_onclick(self, event, exercise):
-        exercise.cmb_video_link['values'] = self.model.get_exercise_video_links(exercise.cmb_exercise_name.get())
-        if exercise.cmb_video_link['values']:
-            exercise.cmb_video_link.current(0)
-        else:
-            exercise.cmb_video_link.set('')
-
-    def _bind_exercise_view_link_button(self, exercise):
-        exercise.btn_view_link.bind('<Button>', lambda event, row=exercise: self._view_onclick(event, row))
-
-    def _bind_exercise_clear_button(self, exercise):
-        exercise.btn_clear.bind('<Button>', lambda event, row=exercise: self._clear_onclick(event, row))
-
     def run(self):
         self.root.title('KELLEY\'S WORKOUT BUILDER')
         self.root.mainloop()
-
-    def _view_onclick(self, event, exercise):
-        webbrowser.open_new(exercise.cmb_video_link.get())
-
-    def _clear_onclick(self, event, exercise):
-        exercise.cmb_classification.set('')
-        exercise.cmb_exercise_name.set('')
-        exercise.spn_sets.delete(0, 'end')
-        exercise.spn_sets.insert(0, 0)
-        exercise.spn_reps.delete(0, 'end')
-        exercise.spn_reps.insert(0, 0)
-        exercise.spn_intensity.delete(0, 'end')
-        exercise.spn_intensity.insert(0, 0)
-        exercise.cmb_video_link.set('')
-
-        self._populate_exercise_comboboxes(exercise)
 
 
 if __name__ == '__main__':
